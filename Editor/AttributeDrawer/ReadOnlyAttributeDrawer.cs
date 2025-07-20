c3aaaf616296de2c38c77584f3b38d3412df0d5c
墨山灼海 <1009840925@qq.com>
2025-07-21 05:14:28 +0800

    [运行时]添加了【只读特性】，[编辑器]添加了对应的【只读特性绘制器】。

---
+++ Editor/AttributeDrawer/ReadOnlyAttributeDrawer.cs
@@ -0,0 +1,56 @@
+#if UNITY_EDITOR
+using MoShan.Unity.EngineExpand;
+using UnityEditor;
+using UnityEngine;
+
+namespace MoShan.Unity.EditorExpand
+{
+    using Rect = global::UnityEngine.Rect;
+
+    /// <summary>
+    /// 属性绘制器：只读特性
+    /// </summary>
+    /// <remarks>
+    /// <para/>用于在【<see cref="global::UnityEditor.InspectorWindow">检视窗口</see>】中显示自定义变量名称
+    /// <para>
+    /// <br><b>注意：</b></br>
+    /// <br>该类型所在的脚本文件需要放置在【Editor】文件夹下；</br>
+    /// <br>该类型需要配合【<see cref="ReadOnlyAttribute">只读特性</see>】类型使用，且对应类型所在的脚本文件应放置在非【Editor】文件夹下。</br>
+    /// </para>
+    /// </remarks>
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))] // 设置自定义属性绘制器对应的类型
+    internal sealed class ReadOnlyAttributeDrawer : PropertyDrawer
+    {
+        #region 生命周期函数
+        /// <summary>
+        /// 绘制 GUI 时
+        /// </summary>
+        /// <param name="position">绘制区域</param>
+        /// <param name="property">绘制属性</param>
+        /// <param name="label">绘制属性的标签</param>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // 禁用 GUI 交互
+            GUI.enabled = false;
+
+            // 绘制【属性字段】
+            EditorGUI.PropertyField(position, property, label, true);
+
+            // 禁用 GUI 交互
+            GUI.enabled = true;
+        }
+
+        /// <summary>
+        /// 获取【属性高度】
+        /// </summary>
+        /// <param name="property">序列化属性</param>
+        /// <param name="label">GUI内容</param>
+        /// <returns>返回属性在【<see cref="global::UnityEditor.InspectorWindow">检视窗口</see>】的绘制高度</returns>
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label);
+        }
+        #endregion
+    }
+}
+#endif
---
+++ Runtime/Attribute/ReadOnlyAttribute.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+namespace MoShan.Unity.EngineExpand
+{
+    /// <summary>
+    /// 只读特性
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// <br><b>使用：</b></br>
+    /// <br>使用该特性标记的【字段】将在检视窗口中禁止进行编辑。</br>
+    /// </para>
+    /// <para>
+    /// <br><b>注意：</b></br>
+    /// <br>该类型的特性标签对字段进行标记时，应位于其它 Unity 原生特性标签之前，否则特性效果可能不会生效。</br>
+    /// <br>该类型所在的脚本文件应放置在非【Editor】文件夹下，否则会因为找不到该类型而导致报错。</br>
+    /// </para>
+    /// </remarks>
+    /// <example>
+    /// <code><![CDATA[
+    /// 
+    /// [ReadOnly]
+    /// public string Example = string.Empty;
+    /// 
+    /// ]]></code>
+    /// </example>
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)] // 仅对字段生效，不可继承，单个字段上不允许添加多个该属性
+    public sealed class ReadOnlyAttribute : PropertyAttribute
+    {
+        #region 构造函数
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public ReadOnlyAttribute() { }
+        #endregion
+    }
+}
