639a6e69c1a4192574cd298605616cf58dad9b96
墨山灼海 <1009840925@qq.com>
2025-08-06 23:51:42 +0800

    [运行时]添加了【双方向】枚举，【GUI拖拽区域绘制】实用程序中的控件绘制。

---
+++ Runtime/Enum/Direction2.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MoShan.Unity.EngineExpand
+{
+    /// <summary>
+    /// 双方向
+    /// </summary>
+    [Serializable]
+    public enum Direction2 : byte
+    {
+        /// <summary>
+        /// 水平
+        /// </summary>
+        Horizontal = 0,
+        /// <summary>
+        /// 垂直
+        /// </summary>
+        Vertical   = 1,
+    }
+}
