17709ae7823ff57315ab9b94b8d5fe6497eaa24f
墨山灼海 <1009840925@qq.com>
2026-06-03 23:07:59 +0800

    [编辑器]修改了部分注释。

--- Editor/OriginalInspectorEditor/SubInspector/TransformInspectorEditor.cs
+++ Editor/OriginalInspectorEditor/SubInspector/TransformInspectorEditor.cs
@@ -1,25 +1,27 @@
 #if UNITY_EDITOR
 using System;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace MoShan.Unity.EditorExpand
 {
-    using Vector3    = global::UnityEngine.Vector3;
     using Quaternion = global::UnityEngine.Quaternion;
     using Transform  = global::UnityEngine.Transform;
+    using Vector3    = global::UnityEngine.Vector3;
 
     /// <summary>
-    /// 检视窗口编辑器：【<see cref="UnityEngine.Transform">变换</see>】
+    /// 检视窗口编辑器：<see cref="UnityEngine.Transform">变换</see>
     /// </summary>
     /// <remarks>
-    /// <para>用于自定义【<see cref="UnityEngine.Transform">变换组件</see>】在【<see cref="global::UnityEditor.InspectorWindow">检视窗口</see>】的显示。</para>
-    /// <para>
-    /// <br><b>注意：</b></br>
-    /// <br>该类型所在的脚本文件应放在【Editor】文件夹下。</br>
-    /// </para>
+    /// 用于自定义 <see cref="UnityEngine.Transform">变换组件</see> 在 <see cref="global::UnityEditor.InspectorWindow">检视窗口</see> 的显示。
+    /// <para/>
+    /// <b>注意：</b>
+    /// <br/>
+    /// 该类型所在的脚本文件应放在 Editor 文件夹下。
     /// </remarks>
     [CanEditMultipleObjects]
     [CustomEditor(typeof(Transform))]
@@ -126,13 +128,13 @@ namespace MoShan.Unity.EditorExpand
 
         #region 静态私有方法
         /// <summary>
-        /// 绘制【复制按钮】
+        /// 绘制复制按钮
         /// </summary>
-        /// <param name="copyContent">复制内容</param>
-        /// <param name="buttonContent">按钮内容</param>
+        /// <param name="copyContent">指定需要复制的内容。</param>
+        /// <param name="buttonContent">指定需要绘制的按钮的内容。</param>
         private static void DrawCopyButton(Vector3 copyContent, GUIContent buttonContent)
         {
-            // 绘制【复制】按钮，并判断 <该按钮是否被触发>
+            // 绘制复制按钮，并判断 <该按钮是否被触发>
             if (GUILayout.Button(buttonContent))
             {
                 // 添加到剪贴板
@@ -141,19 +143,19 @@ namespace MoShan.Unity.EditorExpand
         }
 
         /// <summary>
-        /// 绘制【重置按钮】
+        /// 绘制重置按钮
         /// </summary>
         /// <param name="currentValue">当前值</param>
         /// <param name="defaultValue">默认值</param>
-        /// <param name="buttonContent">按钮内容</param>
         /// <param name="result">返回值</param>
-        /// <returns>返回是否触发重置按钮的判断结果</returns>
+        /// <param name="buttonContent">按钮内容</param>
+        /// <returns>返回值为是否触发重置按钮的判断结果。</returns>
         private static bool DrawResetButton(Vector3 currentValue, Vector3 defaultValue, out Vector3 result, GUIContent buttonContent)
         {
-            // 判断 <【当前值】是否不等于【默认值】>，即<【当前值】是否已发生变更>
+            // 判断 <输入当前值是否不等于输入默认值>，即<输入当前值是否已发生变更>
             if (!currentValue.Equals(defaultValue))
             {
-                // 绘制【重置】按钮，并判断 <该按钮是否被触发>
+                // 绘制重置按钮，并判断 <该按钮是否被触发>
                 if (GUILayout.Button(buttonContent))
                 {
                     result = defaultValue;
@@ -178,10 +180,11 @@ namespace MoShan.Unity.EditorExpand
         }
 
         /// <summary>
-        /// 获取【三维向量字符串】
+        /// 获取三维向量字符串
         /// </summary>
         /// <param name="value">需要转换为字符串的三维向量值</param>
-        /// <returns>获取输入三维向量的字符串值</returns>
+        /// <returns>返回值为 <paramref name="value"/> 的字符串。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static string GetVector3String(Vector3 value)
         {
             // 格式：Vector3(x,y,z)
@@ -189,16 +192,16 @@ namespace MoShan.Unity.EditorExpand
         }
 
         /// <summary>
-        /// 尝试解析【三维向量】
+        /// 尝试解析三维向量
         /// </summary>
-        /// <param name="content">需要进行解析的内容</param>
-        /// <param name="result">解析后返回的内容</param>
-        /// <returns>返回尝试解析是否成功的判断结果</returns>
+        /// <param name="content">指定需要进行解析的内容。</param>
+        /// <param name="result">返回尝试解析后获取的结果。</param>
+        /// <returns>返回值为尝试解析 <paramref name="content"/> 为三维向量是否成功的判断结果。</returns>
         private static bool TryParseVector3(string content, out Vector3 result)
         {
             result = Vector3.zero;
 
-            // 使用正则表达式匹配【x, y, z】的字符串
+            // 使用正则表达式匹配 x, y, z 的字符串
             Match match = Regex.Match(content, @"^Vector3\(\s*(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?)\s*\)$");
 
             // 判断 <正则表达式是否未匹配成功>
@@ -229,13 +232,13 @@ namespace MoShan.Unity.EditorExpand
         }
 
         /// <summary>
-        /// 尝试解析【三维向量】
+        /// 尝试解析三维向量
         /// </summary>
-        /// <param name="xString">需要进行解析的 X 轴内容</param>
-        /// <param name="yString">需要进行解析的 Y 轴内容</param>
-        /// <param name="zString">需要进行解析的 Z 轴内容</param>
-        /// <param name="result">解析后返回的内容</param>
-        /// <returns>返回尝试解析是否成功的判断结果</returns>
+        /// <param name="xString">指定需要解析的三维向量的 X 轴内容。</param>
+        /// <param name="yString">指定需要解析的三维向量的 Y 轴内容。</param>
+        /// <param name="zString">指定需要解析的三维向量的 Z 轴内容。</param>
+        /// <param name="result">返回尝试解析 <paramref name="xString"/>、<paramref name="yString"/> 和 <paramref name="zString"/> 后获取的结果。</param>
+        /// <returns>返回值为尝试解析 <paramref name="xString"/>、<paramref name="yString"/> 和 <paramref name="zString"/> 为三维向量是否成功的判断结果。</returns>
         private static bool TryParseVector3(string xString, string yString, string zString, out Vector3 result)
         {
             result = Vector3.zero;
@@ -256,20 +259,20 @@ namespace MoShan.Unity.EditorExpand
         }
 
         /// <summary>
-        /// 尝试解析【所有三维向量】
+        /// 尝试解析所有三维向量
         /// </summary>
-        /// <param name="content">需要进行解析的内容</param>
-        /// <param name="position">解析后返回的位置</param>
-        /// <param name="rotation">解析后返回的旋转</param>
-        /// <param name="scale">解析后返回的缩放</param>
-        /// <returns>返回尝试解析是否成功的判断结果</returns>
+        /// <param name="content">指定需要进行解析的三维向量的内容。</param>
+        /// <param name="position">返回尝试解析后获取的位置。</param>
+        /// <param name="rotation">返回尝试解析后获取的旋转。</param>
+        /// <param name="scale">返回尝试解析后获取的缩放。</param>
+        /// <returns>返回值为尝试解析 <paramref name="content"/> 是否成功的判断结果。</returns>
         private static bool TryParseVector3s(string content, out Vector3 position, out Vector3 rotation, out Vector3 scale)
         {
             position = Vector3.zero;
             rotation = Vector3.zero;
             scale    = Vector3.zero;
 
-            // 使用正则表达式匹配【x, y, z】的字符串
+            // 使用正则表达式匹配 x, y, z 的字符串
             Match match = Regex.Match(content, @"^Vector3\(\s*(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?)\s*\),Vector3\(\s*(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?)\s*\),Vector3\(\s*(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?)\s*\)$");
 
             // 判断 <正则表达式是否未匹配成功>
@@ -284,7 +287,7 @@ namespace MoShan.Unity.EditorExpand
             string yString = match.Groups[3].Value; // 第二个数字
             string zString = match.Groups[5].Value; // 第三个数字
 
-            // 判断 <尝试解析【位置】是否失败>
+            // 判断 <尝试解析位置是否失败>
             if (!TryParseVector3(xString, yString, zString, out position))
             {
                 position = Vector3.zero;
@@ -298,7 +301,7 @@ namespace MoShan.Unity.EditorExpand
             yString = match.Groups[9].Value;  // 第二个数字
             zString = match.Groups[11].Value; // 第三个数字
 
-            // 判断 <尝试解析【旋转】是否失败>
+            // 判断 <尝试解析旋转是否失败>
             if (!TryParseVector3(xString, yString, zString, out rotation))
             {
                 position = Vector3.zero;
@@ -312,7 +315,7 @@ namespace MoShan.Unity.EditorExpand
             yString = match.Groups[15].Value; // 第二个数字
             zString = match.Groups[17].Value; // 第三个数字
 
-            // 判断 <尝试解析【缩放】是否失败>
+            // 判断 <尝试解析缩放是否失败>
             if (!TryParseVector3(xString, yString, zString, out scale))
             {
                 position = Vector3.zero;
@@ -356,12 +359,15 @@ namespace MoShan.Unity.EditorExpand
         /// <inheritdoc/>
         protected override string OriginalComponentEditorTypeFullName
         {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get
             {
                 return "UnityEditor.TransformInspector";
             }
         }
         #endregion
+
+        // TransformInspector
 
         #region 私有方法
         /// <inheritdoc/>
@@ -380,7 +386,7 @@ namespace MoShan.Unity.EditorExpand
         /// <inheritdoc/>
         protected override void OnEnter()
         {
-            // 从【编辑器首选项】中读取【是否显示扩展功能】
+            // 从编辑器首选项中读取是否显示扩展功能
             m_IsDisplayExtensionFunction = EditorPrefs.GetBool(IS_DISPLAY_EXTENSION_FUNCTION_EDITOR_PREFS_KEY);
         }
 
@@ -390,28 +396,24 @@ namespace MoShan.Unity.EditorExpand
             // 绘制间隔
             GUILayout.Space(10);
 
-            // 绘制【折页】
+            // 绘制折页
             m_IsDisplayExtensionFunction = EditorGUILayout.BeginFoldoutHeaderGroup(m_IsDisplayExtensionFunction, "拓展功能");
 
             // 判断 <是否显示扩展功能>
-            if (!m_IsDisplayExtensionFunction)
+            if (m_IsDisplayExtensionFunction)
             {
-                EditorGUILayout.EndFoldoutHeaderGroup();
+                DrawCopyButton();
 
-                return;
+                DrawPasteButton();
+
+                DrawResetButton();
+
+                DrawAllPropertyCopyButton();
+
+                DrawAllPropertyPasteButton();
+
+                DrawAllPropertyResetButton();
             }
-
-            DrawCopyButton();
-
-            DrawPasteButton();
-
-            DrawResetButton();
-
-            DrawAllPropertyCopyButton();
-
-            DrawAllPropertyPasteButton();
-
-            DrawAllPropertyResetButton();
 
             EditorGUILayout.EndFoldoutHeaderGroup();
         }
@@ -419,16 +421,17 @@ namespace MoShan.Unity.EditorExpand
         /// <inheritdoc/>
         protected override void OnExit()
         {
-            // 将【是否显示扩展功能】存入【编辑器首选项】
+            // 将是否显示扩展功能存入编辑器首选项
             EditorPrefs.SetBool(IS_DISPLAY_EXTENSION_FUNCTION_EDITOR_PREFS_KEY, m_IsDisplayExtensionFunction);
         }
 
         /// <summary>
-        /// 绘制【复制按钮】
+        /// 绘制复制按钮
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void DrawCopyButton()
         {
-            // 判断 <检查器窗口所示的【游戏对象】是否为空值>
+            // 判断 <检查器窗口所示的游戏对象是否为空值>
             if (Selection.activeGameObject == null)
             {
                 return;
@@ -448,11 +451,12 @@ namespace MoShan.Unity.EditorExpand
         }
 
         /// <summary>
-        /// 绘制【粘贴按钮】
+        /// 绘制粘贴按钮
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void DrawPasteButton()
         {
-            // 判断 <检查器窗口所示的【游戏对象】是否为空值>
+            // 判断 <检查器窗口所示的活跃游戏对象是否为空值>
             if (Selection.activeGameObject == null)
             {
                 return;
@@ -460,7 +464,7 @@ namespace MoShan.Unity.EditorExpand
 
             string content = GUIUtility.systemCopyBuffer;
 
-            // 判断 <尝试解析【剪贴板内容】是否成功>，即<内容格式是否为：Vector3(x,y,z)>
+            // 判断 <尝试解析剪贴板内容是否成功>，即<内容格式是否为：Vector3(x,y,z)>
             bool isCanParse = TryParseVector3(GUIUtility.systemCopyBuffer, out Vector3 result);
 
             // 开始水平布局
@@ -472,13 +476,13 @@ namespace MoShan.Unity.EditorExpand
                 // 禁用 GUI 交互
                 GUI.enabled = false;
 
-                // 绘制【粘贴位置】按钮
+                // 绘制粘贴位置按钮
                 GUILayout.Button(s_PositionPasteButtonContent);
 
-                // 绘制【粘贴旋转】按钮
+                // 绘制粘贴旋转按钮
                 GUILayout.Button(s_RotationPasteButtonContent);
 
-                // 绘制【粘贴缩放】按钮
+                // 绘制粘贴缩放按钮
                 GUILayout.Button(s_ScalePasteButtonContent);
 
                 // 启用 GUI 交互
@@ -490,7 +494,7 @@ namespace MoShan.Unity.EditorExpand
                 return;
             }
 
-            // 绘制【粘贴位置】按钮，并判断 <该按钮是否被触发>
+            // 绘制粘贴位置按钮，并判断 <该按钮是否被触发>
             if (GUILayout.Button(s_PositionPasteButtonContent))
             {
                 m_LocalPositionProperty.vector3Value = result;
@@ -498,7 +502,7 @@ namespace MoShan.Unity.EditorExpand
                 ApplyModifiedProperties();
             }
 
-            // 绘制【粘贴旋转】按钮，并判断 <该按钮是否被触发>
+            // 绘制粘贴旋转按钮，并判断 <该按钮是否被触发>
             if (GUILayout.Button(s_RotationPasteButtonContent))
             {
                 m_LocalRotationProperty.quaternionValue = Quaternion.Euler(result);
@@ -506,7 +510,7 @@ namespace MoShan.Unity.EditorExpand
                 ApplyModifiedProperties();
             }
 
-            // 绘制【粘贴缩放】按钮，并判断 <该按钮是否被触发>
+            // 绘制粘贴缩放按钮，并判断 <该按钮是否被触发>
             if (GUILayout.Button(s_ScalePasteButtonContent))
             {
                 m_LocalScaleProperty.vector3Value = result;
@@ -519,14 +523,15 @@ namespace MoShan.Unity.EditorExpand
         }
 
         /// <summary>
-        /// 绘制【重置按钮】
+        /// 绘制重置按钮
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void DrawResetButton()
         {
             // 开始水平布局
             GUILayout.BeginHorizontal();
 
-            // 判断 <是否触发【位置重置】按钮>
+            // 判断 <是否触发位置重置按钮>
             if (DrawResetButton(m_LocalPositionProperty.vector3Value, Vector3.zero, out Vector3 localPosition, s_PositionResetButtonContent))
             {
                 m_LocalPositionProperty.vector3Value = localPosition;
@@ -534,7 +539,7 @@ namespace MoShan.Unity.EditorExpand
                 ApplyModifiedProperties();
             }
 
-            // 判断 <是否触发【旋转重置】按钮>
+            // 判断 <是否触发旋转重置按钮>
             if (DrawResetButton(m_LocalRotationProperty.quaternionValue.eulerAngles, Vector3.zero, out Vector3 localRotation, s_RotationResetButtonContent))
             {
                 m_LocalRotationProperty.quaternionValue = Quaternion.Euler(localRotation);
@@ -542,7 +547,7 @@ namespace MoShan.Unity.EditorExpand
                 ApplyModifiedProperties();
             }
 
-            // 判断 <是否触发【缩放重置】按钮>
+            // 判断 <是否触发缩放重置按钮>
             if (DrawResetButton(m_LocalScaleProperty.vector3Value, Vector3.one, out Vector3 localScale, s_ScaleResetButtonContent))
             {
                 m_LocalScaleProperty.vector3Value = localScale;
@@ -555,11 +560,12 @@ namespace MoShan.Unity.EditorExpand
         }
 
         /// <summary>
-        /// 绘制【所有属性复制按钮】
+        /// 绘制所有属性复制按钮
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void DrawAllPropertyCopyButton()
         {
-            // 绘制【所有属性复制】按钮，并判断 <该按钮是否被触发>
+            // 绘制所有属性复制按钮，并判断 <该按钮是否被触发>
             if (GUILayout.Button(s_AllCopyButtonContent))
             {
                 // 添加到剪贴板
@@ -575,23 +581,24 @@ namespace MoShan.Unity.EditorExpand
         }
 
         /// <summary>
-        /// 绘制【所有属性粘贴按钮】
+        /// 绘制所有属性粘贴按钮
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void DrawAllPropertyPasteButton()
         {
-            // 判断 <解析【系统剪贴板】是否成功>
+            // 判断 <解析系统剪贴板是否成功>
             if (TryParseVector3s(GUIUtility.systemCopyBuffer, out Vector3 position, out Vector3 rotation, out Vector3 scale))
             {
-                // 绘制【所有属性粘贴】按钮，并判断 <该按钮是否被触发>
+                // 绘制所有属性粘贴按钮，并判断 <该按钮是否被触发>
                 if (GUILayout.Button(s_AllPasteButtonContent))
                 {
-                    // 更新【相对位置】
+                    // 更新相对位置
                     m_LocalPositionProperty.vector3Value = position;
 
-                    // 更新【相对旋转】
+                    // 更新相对旋转
                     m_LocalRotationProperty.quaternionValue = Quaternion.Euler(rotation);
 
-                    // 更新【相对缩放】
+                    // 更新相对缩放
                     m_LocalScaleProperty.vector3Value = scale;
 
                     ApplyModifiedProperties();
@@ -610,8 +617,9 @@ namespace MoShan.Unity.EditorExpand
         }
 
         /// <summary>
-        /// 绘制【所有属性重置按钮】
+        /// 绘制所有属性重置按钮
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void DrawAllPropertyResetButton()
         {
             // 判断 <是否存在属性值发生变更>
@@ -619,16 +627,16 @@ namespace MoShan.Unity.EditorExpand
                 m_LocalRotationProperty.quaternionValue != Quaternion.identity ||
                 m_LocalScaleProperty.vector3Value != Vector3.one)
             {
-                // 绘制【所有属性重置】按钮，并判断 <该按钮是否被触发>
+                // 绘制所有属性重置按钮，并判断 <该按钮是否被触发>
                 if (GUILayout.Button(s_AllResetButtonContent))
                 {
-                    // 重置【相对位置】
+                    // 重置相对位置
                     m_LocalPositionProperty.vector3Value = Vector3.zero;
 
-                    // 重置【相对旋转】
+                    // 重置相对旋转
                     m_LocalRotationProperty.quaternionValue = Quaternion.identity;
 
-                    // 重置【相对缩放】
+                    // 重置相对缩放
                     m_LocalScaleProperty.vector3Value = Vector3.one;
 
                     ApplyModifiedProperties();
